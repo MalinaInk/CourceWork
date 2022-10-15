663799bd3df6f2d37384c716a9bb9639b5ba8635
mo <nataleshen.ko@yandex.ru>
2022-10-15 11:05:30 +0300

    CourseWork done

---
+++ src/Employee.java
@@ -0,0 +1,52 @@
+public class Employee {
+    private int salary;
+    private final String fullName;
+    //final - значение можно установить только в конструкторе.
+    //обезопасить от того, чтобы модифицировать переменную случайно.
+    //у final невозможно написать сеттер
+    //одно и то же значение на весь срок жизни объекта
+    //с массивами и объектами не работает, состояние объекта можно поменять, ссылку - нет
+    private final int id;
+    //final - относится к полю объекта
+    //private - относится к полю класса
+    private static int counter = 0;
+
+    public Employee (String fullName, int salary) {
+        this.fullName = fullName;
+        this.salary = salary;
+        this.id = ++counter;
+        //инкрементируем счетчик, начиная с 1-го отдела
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public static void setCounter(int counter) {
+        Employee.counter = counter;
+    }
+
+    @Override
+    public String toString() {
+        return "Зарплата = " + salary +
+                ", Сотрудник = " + fullName +
+                ", Отдел - " + id;
+    }
+   }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,69 @@
 public class Main {
+    private static Employee[] employees = new Employee[10];
+//Объявляем массив вне метода как поле, чтобы сделать его видимым
+//static, т.к. метод main - static
     public static void main(String[] args) {
-        System.out.println("Course Work");
+        //в методе - заполняем
+        employees[0] = new Employee ("Тиунов Георгий", 35_000);
+        employees[1] = new Employee("Сафуанов Артем", 28_000);
+        employees[2] = new Employee("Булдаков Илья", 42_000);
+        employees[3] = new Employee("Горяева Дарья", 58_000);
+        employees[4] = new Employee("Денисенко Светлана", 52_000);
+        employees[5] = new Employee("Гимберг Александр", 32_000);
+        employees[6] = new Employee("Лавринов Вячеслав", 34_000);
+        employees[7] = new Employee("Морева Дарья", 46_000);
+        employees[8] = new Employee("Осипов Андрей", 39_000);
+        employees[9] = new Employee("Ганич Евгения", 51_000);
+        System.out.println("Сумма всех зарплат: " + calculateSumSalary());
+        System.out.println("Средняя зарплата равна " + calculateSumSalary()/employees.length);
+        System.out.println("Сотрудник с максимальной зарплатой: " + getEmployeeWithMaxSalary());
+        System.out.println("Сотрудник с минимальной зарплатой: " + getEmployeeWithMinSalary());
+        printAllEmployees();
+
+    }
+
+    // отдельный статический метод, который работает со статическим полем, объявленным выше
+    public static int calculateSumSalary() {
+        int sum = 0;
+        for (Employee employee: employees) {
+            //проверка на null
+            if (employee != null) {
+                sum += employee.getSalary();
+            }
+        }
+        return sum;
+    }
+
+    public static Employee getEmployeeWithMaxSalary() {
+        int max = Integer.MIN_VALUE;
+        //Задаем минимально допустимое значение внутри типа инт
+        Employee targetEmployee = null;
+        for (Employee employee : employees) {
+            if (employee != null && employee.getSalary() > max) {
+                max = employee.getSalary();
+                targetEmployee = employee;
+            }
+        }
+        return targetEmployee;
+    }
+
+    public static Employee getEmployeeWithMinSalary() {
+        int min = Integer.MAX_VALUE;
+        Employee targetEmployee = null;
+        for (Employee employee : employees) {
+            if (employee != null && employee.getSalary() < min) {
+                min = employee.getSalary();
+                targetEmployee = employee;
+            }
+        }
+        return targetEmployee;
+    }
+
+
+    public static void printAllEmployees() {
+        for (int i = 0; i < 10; i++) {
+            Employee employee = employees[i];
+            System.out.println(employee.toString());
+        }
     }
 }
