13e26bb3ded3553358e249e9c70b3cb1340fae01
mo <nataleshen.ko@yandex.ru>
2022-10-30 21:09:13 +0300

    CourseWork edits3

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,16 @@ public class Main {
 //static, т.к. метод main - static
     public static void main(String[] args) {
         //в методе - заполняем
-        employees[0] = new Employee("Тиунов Георгий", 35_000);
-        employees[1] = new Employee("Сафуанов Артем", 28_000);
-        employees[2] = new Employee("Булдаков Илья", 42_000);
-        employees[3] = new Employee("Горяева Дарья", 58_000);
-        employees[4] = new Employee("Денисенко Светлана", 52_000);
-        employees[5] = new Employee("Гимберг Александр", 32_000);
-        employees[6] = new Employee("Лавринов Вячеслав", 34_000);
-        employees[7] = new Employee("Морева Дарья", 46_000);
-        employees[8] = new Employee("Осипов Андрей", 39_000);
-        employees[9] = new Employee("Ганич Евгения", 51_000);
+        employees[0] = new Employee("Тиунов Георгий", 1,35_000);
+        employees[1] = new Employee("Сафуанов Артем", 2,28_000);
+        employees[2] = new Employee("Булдаков Илья", 3,42_000);
+        employees[3] = new Employee("Горяева Дарья", 4, 58_000);
+        employees[4] = new Employee("Денисенко Светлана", 5,52_000);
+        employees[5] = new Employee("Гимберг Александр", 1,32_000);
+        employees[6] = new Employee("Лавринов Вячеслав", 2,34_000);
+        employees[7] = new Employee("Морева Дарья", 3,46_000);
+        employees[8] = new Employee("Осипов Андрей", 4,39_000);
+        employees[9] = new Employee("Ганич Евгения", 5,51_000);
         System.out.println("Сумма всех зарплат: " + calculateSumSalary());
         System.out.println("Сотрудник с максимальной зарплатой: " + getEmployeeWithMaxSalary());
         System.out.println("Сотрудник с минимальной зарплатой: " + getEmployeeWithMinSalary());
@@ -33,7 +33,7 @@ public class Main {
         }
         return sum;
     }
-    private static int getCountEmployees(Employee employees[]) {
+    private static int getCountEmployees() {
         int countEmployees = 0;
         for (int i = 0; i < employees.length; i++) {
             if (employees[i] != null) {
@@ -42,13 +42,12 @@ public class Main {
         }
         return countEmployees;
     }
-    private static int getAverageSalary() {
-        int averageSalary = calculateSumSalary() / getCountEmployees(employees);
+    private static double getAverageSalary() {
+        double averageSalary = (double) calculateSumSalary() / getCountEmployees();
         return averageSalary;
     }
     private static Employee getEmployeeWithMaxSalary() {
         int max = Integer.MIN_VALUE;
-        //Задаем минимально допустимое значение внутри типа инт
         Employee targetEmployee = null;
         for (Employee employee : employees) {
             if (employee != null && employee.getSalary() > max) {
